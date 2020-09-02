ea1762860f8d405b2e4735fe0b210b4888d422f0
Adam Ackerman <acke9387@PCB-CLRM-2040>
2020-09-02 12:16:00 -0500

    Added an example with a student UML with exception handling

---
+++ Review/StudentExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StudentExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            Console.WriteLine(myStudent);
+            myStudent.FirstName = "Adam";
+            myStudent.LastName = "Ackerman";
+            myStudent.IsOnProbation = true;
+            myStudent.SoonerID = 5;
+            Console.WriteLine(myStudent);
+            Console.WriteLine();
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+            myStudent.MakePayment(50);
+            Console.WriteLine();
+            Console.WriteLine(myStudent);
+
+            try
+            {
+                myStudent.MakePayment(-500);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("INVALID PAYMENT");
+                //throw;
+            }
+            
+            Console.WriteLine();
+
+            Console.WriteLine(myStudent);
+        }
+    }
+}
