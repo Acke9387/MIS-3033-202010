7f8256b649be018343c09c316f158ad6600cb6f2
Adam Ackerman <acke9387@PCB-CLRM-2040>
2020-09-02 11:35:54 -0500

    Created the FileExample project and successfully got it to read in a file and display it chapter by chapter

---
+++ Review/FileExample/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * 
+ * @Author : Professor Ackerman
+ * Collaborated with the entire class
+ */
+using System;
+using System.IO;
+
+namespace FileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string filePath = @"C:\random files\Pride and Prejudice by Jane Austen.txt";
+
+            // Reads the entire file into 1 SINGLE string
+            //string contentsOfFile = File.ReadAllText(filePath);
+
+            var lines = File.ReadAllLines(filePath);
+  
+            //Write a chapter out at a time
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currentLine = lines[i];
+                if (currentLine.Contains("Chapter"))
+                {
+                    Console.ReadKey();
+                }
+
+                Console.WriteLine(currentLine);
+
+            }
+
+            //Output 5 lines at a time of the file
+            //for (int i = 0; i < lines.Length; i++)
+            //{
+            //    if (i % 5 == 0 && i != 0)
+            //    {
+            //        Console.ReadKey();
+            //    }
+
+            //    Console.WriteLine(lines[i]);
+
+            //}
+
+            Console.WriteLine(lines);
+        }
+    }
+}
